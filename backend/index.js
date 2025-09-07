a75836121534be5407b9923415989c08b1ce8160
Vivek Reddy <katamvivek527@gmail.com>
2025-09-07 16:53:16 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -48,6 +48,12 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
