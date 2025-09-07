f21e9a21ce760c69d9540bc642899e7e3eda2ebf
Vivek Reddy <katamvivek527@gmail.com>
2025-09-07 17:06:24 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -48,7 +48,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
 
 app.get("*",(req,res)=>{
   res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
